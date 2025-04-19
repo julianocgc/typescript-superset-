8d8a86337a363bcbc7849134504dc067d26aa345
Juliano <72839366+julianocgc@users.noreply.github.com>
2025-04-19 10:04:51 -0300

    Envio de cóodigos
    Envio de códigos Typescript.

---
+++ aula02.js
@@ -0,0 +1,6 @@
+var nome = "Bruno";
+var canal = "CFBCursos";
+var curso = "TypeScript";
+console.log(nome);
+console.log(canal);
+console.log(curso);
---
+++ aula02.ts
@@ -0,0 +1,7 @@
+const nome = "Bruno";
+const canal = "CFBCursos";
+const curso = "TypeScript";
+
+console.log(nome);
+console.log(canal);
+console.log(curso);
