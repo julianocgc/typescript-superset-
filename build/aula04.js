fd8a6b04f3e9846ff2b58daa9e77385fd6985439
Juliano <72839366+julianocgc@users.noreply.github.com>
2025-04-20 19:57:21 -0300

    Envio de códigos
    Envio de códigos Typescript.

---
+++ build/aula04.js
@@ -0,0 +1,8 @@
+"use strict";
+let vnome;
+vnome = "Bruno";
+vnome = "Pinho";
+let valor = 0;
+valor = 10.5;
+console.log(valor);
+console.log(vnome);
---
+++ src/aula04.ts
@@ -0,0 +1,9 @@
+let vnome : string;
+vnome = "Bruno";
+vnome = "Pinho";
+
+let valor = 0; // int, float, bin, hex
+valor = 10.5;
+
+console.log(valor);
+console.log(vnome);
